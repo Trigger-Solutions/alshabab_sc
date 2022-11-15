de52bec96474837736b511439be63e26a4c3689e
Trigger-Solutions <trigger.solutions.eg@gmail.com>
2022-11-15 17:38:52 +0000

    update3

--- alshabab_sc/internal_system/doctype/requests_tasks/requests_tasks.py
+++ alshabab_sc/internal_system/doctype/requests_tasks/requests_tasks.py
@@ -3,13 +3,24 @@
 
 import frappe
 from frappe.model.document import Document
-
 class RequestsTasks(Document):
 	def on_submit(self):
-		# frappe.db.get_doc("Requests",self.sequests)
 		frappe.db.set_value('Concerned Departments',self.refrence_name,{
 				'comment':self.replay,
 				'action':"Submitted"
 			})
+		doc = frappe.get_doc("Requests",self.requests)
+		if doc.concerned_departments is not None:
+			status = 1
+			for item in doc.concerned_departments:
+				if item.action =="Draft" :
+					status = 0
+					break
+			if status == 1:
+				frappe.db.set_value('Requests',self.requests,{
+				'workflow_state':"Approved",
+				'docstatus': 1
+				})
+				
 
 	
